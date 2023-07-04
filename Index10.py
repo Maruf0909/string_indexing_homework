e93244ba5967129e1d6dcd97f2341d70a9e49970
Mac <mac@MacBook-Air-Mac.local>
2023-07-04 15:24:34 +0500

    idx10

--- Index10.py
+++ Index10.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    sum=0
+    if s[0].isdigit():
+        sum+= int(s[0])
+    if s[1].isdigit():
+        sum+= int(s[1])
+    if s[2].isdigit():
+        sum+= int(s[2])
+    if s[3].isdigit():
+        sum+= int(s[3])
+    if s[4].isdigit():
+        sum+= int(s[4])
+
+    return sum
+print(main("10007"))
