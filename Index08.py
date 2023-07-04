3828668f760d6f6f88dbd3adb8f203eb55a20687
Mac <mac@MacBook-Air-Mac.local>
2023-07-04 16:34:26 +0500

    idx8

--- Index08.py
+++ Index08.py
@@ -6,5 +6,15 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s[0]=="*":
+        a=0
+    if s[1]=="*":
+        a=1
+    if s[2]=="*":
+        a=2
+    if s[3]=="*":
+        a=3
+    
+    return s[0]=="*" and a or s[1]=="*" and a or s[2]=="*" and a or s[3]=="*" and a 
+print(main("g**d"))
         
