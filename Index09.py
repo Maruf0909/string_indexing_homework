46f93b92b686b9889b679416aecf43a3f9d86d27
Mac <mac@MacBook-Air-Mac.local>
2023-07-04 15:00:28 +0500

    idx9

--- Index09.py
+++ Index09.py
@@ -1,9 +1,13 @@
 def main(s):
     """
-    a single character string is given. Convert it to int type, return -1 if not possible.
+    Given a variable s string of length five. Determine the number of digits involved in this variable.
     Args:
         s(str): parameter
     Returns:
         int: answer
     """
-    return
+    if s[0]=="0" or s[0]=="1" or s[0]=="2" or s[0]=="3" or s[0]=="4" or s[0]=="5" or s[0]=="6":
+        return int(s)
+    
+    return -1
+print(main("4"))
