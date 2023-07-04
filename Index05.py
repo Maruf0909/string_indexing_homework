994f8a964bf625b2559bee710060de865c77939c
Mac <mac@MacBook-Air-Mac.local>
2023-07-04 14:33:16 +0500

    idx5

--- Index05.py
+++ Index05.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    countr=0
+
+    if s[0].isdigit():
+        countr+=1
+    if s[1].isdigit():
+        countr+=1
+    if s[2].isdigit():
+        countr+=1
+    if s[3].isdigit():
+        countr+=1
+    if s[4].isdigit():
+        countr+=1
+    return countr 
+print(main("1xs40"))
